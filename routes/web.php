1bd391cf01b37f57fafd4b02a93b720c29674ad7
Arup <arupctg@gmail.com>
2020-05-19 23:22:31 +0600

    blade templating

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+Route::get('/','Backend\SampleController@index');
+Route::get('/welcome','Backend\SampleController@welcome');
+Route::get('/posts','Backend\SampleController@posts');
+
+
